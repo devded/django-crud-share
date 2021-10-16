2594ee266bf26b55be83b6fdd30fa33250b1117a
Dedar <thededar@gmail.com>
2021-10-16 22:34:09 +0600

    update

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -23,9 +23,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Random Practice API",
       default_version='v1',
-      description="Test description",
+      description="This is project for educational perpose",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -43,7 +43,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/todo/', include('todo.urls')),
     path('api/v1/price/',include('price.urls')),
-    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
