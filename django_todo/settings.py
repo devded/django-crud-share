4f6f6a41f969bfff99d5e032420dd148a9b79651
Dedar <thededar@gmail.com>
2021-11-02 23:19:09 +0600

    update

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure--$7pvs5w$@i94m&w5vrs6@+_*bt=0kmjeyxz!-yz8=@d304$se
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "0.0.0.0"]
+ALLOWED_HOSTS = ["*", "0.0.0.0",]
 
 
 # Application definition
@@ -143,5 +143,33 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     'http://localhost:3000',
+# ]
+# CORS_ALLOW_METHODS = [
+#     "DELETE",
+#     "GET",
+#     "OPTIONS",
+#     "PATCH",
+#     "POST",
+#     "PUT",
+# ]
+# CORS_ALLOW_HEADERS = [
+#     "accept",
+#     "accept-encoding",
+#     "authorization",
+#     "content-type",
+#     "dnt",
+#     "origin",
+#     "user-agent",
+#     "x-csrftoken",
+#     "x-requested-with",
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 django_heroku.settings(locals())
